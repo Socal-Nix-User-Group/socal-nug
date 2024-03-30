0b856d8f506dd1c37f71183df1ef5c2a7e8242d7
David Nuon <davidnuongm@gmail.com>
2024-03-30 10:55:33 -0700

    Add ability to handle multiple posts

--- scripts/verify_post.py
+++ scripts/verify_post.py
@@ -6,6 +6,18 @@ import click
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
+# https://svn.blender.org/svnroot/bf-blender/trunk/blender/build_files/scons/tools/bcolors.py
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 @click.command()
 @click.argument('post',  nargs=-1)
 @click.option('--schema', help='Path to the Event JSON Schema file')
@@ -17,7 +29,6 @@ def validate_post(post, schema):
         schema = json.loads(schema_content)
 
     for post in post:
-        print('Validating post...', post, ' ', end="")
 
         with open(post, 'r') as f:
             post_content = f.read().strip()
@@ -27,10 +38,10 @@ def validate_post(post, schema):
         data = tomllib.loads(front_matter)
         try:
             validate(instance=data, schema=schema)
-            print('OK')
+            print(f'{bcolors.OKGREEN}[OK]{bcolors.ENDC} {post}')
         except ValidationError as e:
-            print('Error: Invalid front matter')
-            print(e)
+            print(f'{bcolors.FAIL}[ERROR]{bcolors.ENDC} {post}')
+            print(f"\tInvalid front matter {e.message} at {e.json_path.replace('$', 'Root')}")
             any_failed = True
 
     if any_failed:
