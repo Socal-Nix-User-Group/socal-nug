dbdb7faf3fb94c5d553751ec7a11376108e555cc
David Nuon <davidnuongm@gmail.com>
2024-03-30 10:29:31 -0700

    Add ability to verify multiple documents

--- scripts/verify_event.py
+++ scripts/verify_event.py
@@ -7,28 +7,35 @@ from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
 @click.command()
-@click.option('--post', help='Path to the Markdown file')
+@click.argument('post',  nargs=-1)
 @click.option('--schema', help='Path to the Event JSON Schema file')
 def validate_post(post, schema):
-    print('Validating post...', post, ' ', end="")
+    any_failed = False
 
-    with open(post, 'r') as f:
-        post_content = f.read().strip()
-    
     with open(schema, 'r') as f:
         schema_content = f.read().strip()
         schema = json.loads(schema_content)
 
-    separator = '+++'
-    front_matter = post_content.split(separator)[1].strip()
-    data = tomllib.loads(front_matter)
-    try:
-        validate(instance=data, schema=schema)
-        print('OK')
-    except ValidationError as e:
-        print('Error: Invalid front matter')
-        print(e)
-        exit(0)
+    for post in post:
+        print('Validating post...', post, ' ', end="")
+
+        with open(post, 'r') as f:
+            post_content = f.read().strip()
+        
+        separator = '+++'
+        front_matter = post_content.split(separator)[1].strip()
+        data = tomllib.loads(front_matter)
+        try:
+            validate(instance=data, schema=schema)
+            print('OK')
+        except ValidationError as e:
+            print('Error: Invalid front matter')
+            print(e)
+            any_failed = True
+
+    if any_failed:
+        exit(1)
+        
 
 
 if __name__ == '__main__':
