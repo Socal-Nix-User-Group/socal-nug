6841755b3e2de3132ee1a49359b033284e2e3ad2
Daniel Baker <daniel.n.baker@gmail.com>
2025-01-18 14:11:52 -0800

    meta: update flake inputs
    Updates flake inputs. Clean flake file. Update flake lock.

--- flake.nix
+++ flake.nix
@@ -2,22 +2,23 @@
   description = "Southern California Nix User Group Site";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
-  inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         packages = with pkgs; rec {
           socalNixSite = stdenv.mkDerivation {
             name = "socal-nix-site";
